30b8122ccc5a1054839f3a0df2e0ef288ac6d98c
9Neechan <st102678@student.spbu.ru>
2024-03-23 09:21:33 +0300

    Added yaml file

--- go.mod
+++ go.mod
@@ -2,12 +2,11 @@ module task
 
 go 1.21.1
 
-require go.mongodb.org/mongo-driver v1.14.0
-
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- main.go
+++ main.go
@@ -3,18 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
-	"task/internal/flood_control"
+	floodcontrol "task/internal/flood_control"
+
+	"gopkg.in/yaml.v3"
 )
 
+type TestParams struct {
+	UserID_from  int
+	UserID_to    int
+	Num_requests int
+}
+
 func main() {
+	var test_params TestParams
+
+	yamlFile, err := os.ReadFile("test_config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &test_params)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fc := floodcontrol.NewFC(context.Background())
 
-	for userID := 215; userID < 220; userID++ {
-		for i := 0; i < 15; i++ {
+	for userID := test_params.UserID_from; userID < test_params.UserID_to; userID++ {
+		for i := 0; i < test_params.Num_requests; i++ {
 			// первые 10 запросов пройдут проверку, остальные 5 - нет
-			ans, err := fc.Check(context.Background(),int64(userID))
+			ans, err := fc.Check(context.Background(), int64(userID))
 			fmt.Println(i, ans, err)
 			time.Sleep(time.Second) // 1 sec
 		}
